755abbd17ac71d2947fe8bc1465b5609237ed1f1
obiwan7713 <joshcummings1991@gmail.com>
2016-04-10 20:34:03 -0400

    finished cipher

---
+++ cipher.rb
@@ -0,0 +1,27 @@
+def cipher(string, key)
+
+	key = key.to_i
+	letters = string.split("")
+	numbers = letters.map {|x| x.ord}
+	new_letters = numbers.map do |y|
+		
+		if y <= 90 && y >= 65
+			(((y - 65 + key) % 26) + 65).chr
+		elsif y <= 122 && y >= 97
+			(((y - 97 + key) % 26) + 97).chr
+		else
+			y.chr
+		end
+	end
+	
+	puts "The encrypted code is #{new_letters.join}"
+end
+
+puts "Enter the text you would like to encrypt."
+string = gets.chomp
+puts "Enter the key to your code."
+key = gets.chomp
+cipher(string, key)
+
+
+
